82be13c7eafbd420a6fd8e85f69e193cdb2ed824
Dan Goldin <dangoldin@gmail.com>
2012-11-10 15:44:49 -0500

    Added percentage code

---
+++ craigslist/templatetags/layout.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django import template
+#from django.template.defaultfilters import floatformat
+
+import logging
+logger = logging.getLogger(__name__)
+
+register = template.Library()
+
+@register.filter
+def percent(value):
+    if value is None:
+        return None
+    return str(int(value * 100)) + '%'
